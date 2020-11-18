b0a26233ae363489aa935bfd4e4e2786864fb5ff
Augusto Bardini <azbardini@gmail.com>
2020-11-17 21:37:18 -0300

    Arruma o plano

--- src/main.cpp
+++ src/main.cpp
@@ -142,6 +142,8 @@ struct SceneObject
     size_t       num_indices; // Número de índices do objeto dentro do vetor indices[] definido em BuildTrianglesAndAddToVirtualScene()
     GLenum       rendering_mode; // Modo de rasterização (GL_TRIANGLES, GL_TRIANGLE_STRIP, etc.)
     GLuint       vertex_array_object_id; // ID do VAO onde estão armazenados os atributos do modelo
+    glm::vec3    bbox_min; // Axis-Aligned Bounding Box do objeto
+    glm::vec3    bbox_max;
 };
 
 // Abaixo definimos variáveis globais utilizadas em várias funções do código.
@@ -199,8 +201,10 @@ GLint model_uniform;
 GLint view_uniform;
 GLint projection_uniform;
 GLint object_id_uniform;
+GLint bbox_min_uniform;
+GLint bbox_max_uniform;
 
-glm::vec4 firstCameraPos  = glm::vec4(1.0f, 1.0f, 4.0f, 1.0f);
+glm::vec4 firstCameraPos  = glm::vec4(1.0f, 1.0f, -4.0f, 1.0f);
 glm::vec4 cameraPos  = firstCameraPos;
 
 static int cosAngle(glm::vec4 v1, glm::vec4 v2);
@@ -210,7 +214,6 @@ GLuint g_NumLoadedTextures = 0;
 
 // Estados da maquina
 glm::vec4 cameraTarget;
-
 int throwBunny = 0;
 glm::vec4 throwBunnyVector;
 
@@ -288,8 +291,9 @@ int main(int argc, char* argv[])
     LoadShadersFromFiles();
 
     // Carregamos duas imagens para serem utilizadas como textura
-    LoadTextureImage("../../data/tc-earth_daymap_surface.jpg");      // TextureImage0
-    LoadTextureImage("../../data/tc-earth_nightmap_citylights.gif"); // TextureImage1
+    LoadTextureImage("../../data/grass.jpg");      // TextureImage0
+    LoadTextureImage("../../data/tc-earth_daymap_surface.jpg");      // TextureImage1
+    LoadTextureImage("../../data/tc-earth_nightmap_citylights.gif"); // TextureImage2
 
     // Construímos a representação de objetos geométricos através de malhas de triângulos
     ObjModel cowModel("../../data/cow.obj");
@@ -476,7 +480,6 @@ int main(int argc, char* argv[])
             throwBunny = 0;
         }
 
-
         // Desenhamos o modelo do plano
         model = Matrix_Translate(0.0f,-1.0f,0.0f) 
               * Matrix_Scale(20.0f,1.0f,20.0f);
@@ -588,6 +591,13 @@ void DrawVirtualObject(const char* object_name)
     // comentários detalhados dentro da definição de BuildTrianglesAndAddToVirtualScene().
     glBindVertexArray(g_VirtualScene[object_name].vertex_array_object_id);
 
+    // Setamos as variáveis "bbox_min" e "bbox_max" do fragment shader
+    // com os parâmetros da axis-aligned bounding box (AABB) do modelo.
+    glm::vec3 bbox_min = g_VirtualScene[object_name].bbox_min;
+    glm::vec3 bbox_max = g_VirtualScene[object_name].bbox_max;
+    glUniform4f(bbox_min_uniform, bbox_min.x, bbox_min.y, bbox_min.z, 1.0f);
+    glUniform4f(bbox_max_uniform, bbox_max.x, bbox_max.y, bbox_max.z, 1.0f);
+
     // Pedimos para a GPU rasterizar os vértices dos eixos XYZ
     // apontados pelo VAO como linhas. Veja a definição de
     // g_VirtualScene[""] dentro da função BuildTrianglesAndAddToVirtualScene(), e veja
@@ -645,6 +655,15 @@ void LoadShadersFromFiles()
     view_uniform            = glGetUniformLocation(program_id, "view"); // Variável da matriz "view" em shader_vertex.glsl
     projection_uniform      = glGetUniformLocation(program_id, "projection"); // Variável da matriz "projection" em shader_vertex.glsl
     object_id_uniform       = glGetUniformLocation(program_id, "object_id"); // Variável "object_id" em shader_fragment.glsl
+    bbox_min_uniform        = glGetUniformLocation(program_id, "bbox_min");
+    bbox_max_uniform        = glGetUniformLocation(program_id, "bbox_max");
+
+    // Variáveis em "shader_fragment.glsl" para acesso das imagens de textura
+    glUseProgram(program_id);
+    glUniform1i(glGetUniformLocation(program_id, "TextureImage0"), 0);
+    glUniform1i(glGetUniformLocation(program_id, "TextureImage1"), 1);
+    glUniform1i(glGetUniformLocation(program_id, "TextureImage2"), 2);
+    glUseProgram(0);
 }
 
 // Função que pega a matriz M e guarda a mesma no topo da pilha
@@ -749,6 +768,12 @@ void BuildTrianglesAndAddToVirtualScene(ObjModel* model)
         size_t first_index = indices.size();
         size_t num_triangles = model->shapes[shape].mesh.num_face_vertices.size();
 
+        const float minval = std::numeric_limits<float>::min();
+        const float maxval = std::numeric_limits<float>::max();
+
+        glm::vec3 bbox_min = glm::vec3(maxval,maxval,maxval);
+        glm::vec3 bbox_max = glm::vec3(minval,minval,minval);
+
         for (size_t triangle = 0; triangle < num_triangles; ++triangle)
         {
             assert(model->shapes[shape].mesh.num_face_vertices[triangle] == 3);
@@ -767,6 +792,13 @@ void BuildTrianglesAndAddToVirtualScene(ObjModel* model)
                 model_coefficients.push_back( vy ); // Y
                 model_coefficients.push_back( vz ); // Z
                 model_coefficients.push_back( 1.0f ); // W
+
+                bbox_min.x = std::min(bbox_min.x, vx);
+                bbox_min.y = std::min(bbox_min.y, vy);
+                bbox_min.z = std::min(bbox_min.z, vz);
+                bbox_max.x = std::max(bbox_max.x, vx);
+                bbox_max.y = std::max(bbox_max.y, vy);
+                bbox_max.z = std::max(bbox_max.z, vz);
 
                 // Inspecionando o código da tinyobjloader, o aluno Bernardo
                 // Sulzbach (2017/1) apontou que a maneira correta de testar se
@@ -802,6 +834,9 @@ void BuildTrianglesAndAddToVirtualScene(ObjModel* model)
         theobject.num_indices    = last_index - first_index + 1; // Número de indices
         theobject.rendering_mode = GL_TRIANGLES;       // Índices correspondem ao tipo de rasterização GL_TRIANGLES.
         theobject.vertex_array_object_id = vertex_array_object_id;
+
+        theobject.bbox_min = bbox_min;
+        theobject.bbox_max = bbox_max;
 
         g_VirtualScene[model->shapes[shape].name] = theobject;
     }
--- src/shader_fragment.glsl
+++ src/shader_fragment.glsl
@@ -7,20 +7,42 @@
 in vec4 position_world;
 in vec4 normal;
 
+// Posição do vértice atual no sistema de coordenadas local do modelo.
+in vec4 position_model;
+
+// Coordenadas de textura obtidas do arquivo OBJ (se existirem!)
+in vec2 texcoords;
+
 // Matrizes computadas no código C++ e enviadas para a GPU
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 // Identificador que define qual objeto está sendo desenhado no momento
-#define SPHERE 0
+#define COW 0
 #define BUNNY  1
 #define PLANE  2
 uniform int object_id;
 
+// Parâmetros da axis-aligned bounding box (AABB) do modelo
+uniform vec4 bbox_min;
+uniform vec4 bbox_max;
+
+// Variáveis para acesso das imagens de textura
+uniform sampler2D TextureImage0;
+uniform sampler2D TextureImage1;
+uniform sampler2D TextureImage2;
+
 // O valor de saída ("out") de um Fragment Shader é a cor final do fragmento.
 out vec3 color;
 
+// Constantes
+#define M_PI   3.14159265358979323846
+#define M_PI_2 1.57079632679489661923
+
+
+vec4 P;
+vec4 C;
 void main()
 {
     // Obtemos a posição da câmera utilizando a inversa da matriz que define o
@@ -47,20 +69,39 @@ void main()
 
     // Vetor que define o sentido da reflexão especular ideal.
     vec4 r = -l + 2*n*dot(n,l); 
-    
-    // Parâmetros que definem as propriedades espectrais da superfície
-    vec3 Kd; // Refletância difusa
-    vec3 Ks; // Refletância especular
-    vec3 Ka; // Refletância ambiente
-    float q; // Expoente especular para o modelo de iluminação de Phong
 
-    if ( object_id == SPHERE )
+    // Coordenadas de textura U e V
+    float U = 0.0;
+    float V = 0.0;
+
+    // Parâmetros que definem as propriedades espectrais da superfície
+    vec3 Kd = vec3(0.0,0.0,0.0); // Refletância difusa
+    vec3 Ks = vec3(0.0,0.0,0.0); // Refletância especular
+    vec3 Ka = vec3(0.0,0.0,0.0); // Refletância ambiente
+    float q = 1; // Expoente especular para o modelo de iluminação de Phong
+
+    if ( object_id == COW )
     {
         // Propriedades espectrais da esfera
         Kd = vec3(0.8, 0.4, 0.08);
-        Ks = vec3(0.0,0.0,0.0);
         Ka = vec3(0.4, 0.2, 0.04);
         q = 1.0;
+
+
+        vec4 bbox_center = (bbox_min + bbox_max) / 2.0;
+        float rho = 1.0;
+        P = position_model; 
+        C = bbox_center; 
+        
+        vec4 pLine = C + (rho * ((P-C)/length(P-C)));
+        vec4 pVect = pLine - C;
+
+        float phi = asin(pVect.y/rho);
+        float theta = atan(pVect.x, pVect.z);
+
+        V = (phi + M_PI_2) / M_PI;
+        U = (theta + M_PI) / (2 * M_PI);
+
     }
     else if ( object_id == BUNNY )
     {
@@ -69,62 +110,75 @@ void main()
         Ks = vec3(0.8, 0.8, 0.8);
         Ka = vec3(0.04, 0.2, 0.4);
         q = 32.0;
+
+        // As coordenadas de textura do coelho, computadas com
+        // projeção planar XY em COORDENADAS DO MODELO. Utilize como referência
+        // o slides 99-104 do documento Aula_20_Mapeamento_de_Texturas.pdf,
+        // e também use as variáveis min*/max* definidas abaixo para normalizar
+        // as coordenadas de textura U e V dentro do intervalo [0,1]. Para
+        // tanto, veja por exemplo o mapeamento da variável 'p_v' utilizando
+        // 'h' no slides 158-160 do documento Aula_20_Mapeamento_de_Texturas.pdf.
+        // Veja também a Questão 4 do Questionário 4 no Moodle.
+        float minx = bbox_min.x;
+        float maxx = bbox_max.x;
+        float miny = bbox_min.y;
+        float maxy = bbox_max.y;
+        float minz = bbox_min.z;
+        float maxz = bbox_max.z;
+        P = position_model; 
+        U = (P.x-minx)/(maxx-minx);
+        V = (P.y-miny)/(maxy-miny);
     }
     else if ( object_id == PLANE )
     {
-        // Propriedades espectrais do plano
+        // Coordenadas de textura do plano, obtidas do arquivo OBJ.
+        U = texcoords.x;
+        V = texcoords.y;
+
+       // Propriedades espectrais do plano
         Kd = vec3(0.2, 0.2, 0.2);
         Ks = vec3(0.3, 0.3, 0.3);
-        Ka = vec3(0.0,0.0,0.0);
-        q = 20.0;
-    }
-    else // Objeto desconhecido = preto
-    {
-        Kd = vec3(0.0,0.0,0.0);
-        Ks = vec3(0.0,0.0,0.0);
-        Ka = vec3(0.0,0.0,0.0);
-        q = 1.0;
+        q = 10.0;
     }
 
+   // Obtemos a refletância difusa a partir da leitura da imagem TextureImage0
+    vec3 Kd0 = texture(TextureImage0, vec2(U,V)).rgb;
+    vec3 Kd1 = texture(TextureImage1, vec2(U,V)).rgb;
+    vec3 Kd2 = texture(TextureImage2, vec2(U,V)).rgb;
     // Espectro da fonte de iluminação
     vec3 I = vec3(1.0,1.0,1.0); // o espectro da fonte de luz
-
     // Espectro da luz ambiente
     vec3 Ia = vec3(0.2, 0.2, 0.2); // o espectro da luz ambiente
-
     // Termo difuso utilizando a lei dos cossenos de Lambert
     vec3 lambert_diffuse_term = Kd*I*max(0, dot(n,l)); // o termo difuso de Lambert
-
     // Termo ambiente
     vec3 ambient_term =  Ka*Ia;// o termo ambiente
-
     // Termo especular utilizando o modelo de iluminação de Phong
     vec3 phong_specular_term  = Ks*I*pow(max(0, dot(r,v)), q); // o termo especular de Phong
 
+
+    if ( object_id == COW )
+    {
     // Cor final do fragmento calculada com uma combinação dos termos difuso,
     // especular, e ambiente. Veja slide 129 do documento Aula_17_e_18_Modelos_de_Iluminacao.pdf.
-    if ( object_id == SPHERE )
-    {
-        color = lambert_diffuse_term + ambient_term + phong_specular_term;
+    color = Kd1*(lambert_diffuse_term + ambient_term + phong_specular_term);
     }
     else if ( object_id == BUNNY )
     {
-        color = lambert_diffuse_term + ambient_term + phong_specular_term;
+    // Cor final do fragmento calculada com uma combinação dos termos difuso,
+    // especular, e ambiente. Veja slide 129 do documento Aula_17_e_18_Modelos_de_Iluminacao.pdf.
+    color = Kd1*(lambert_diffuse_term + ambient_term + phong_specular_term);
     }
     else if ( object_id == PLANE )
     {
-        color = lambert_diffuse_term + ambient_term + phong_specular_term;
-    }
-    else // Objeto desconhecido = preto
-    {
-        color = lambert_diffuse_term + ambient_term + phong_specular_term;
+    // Cor final do fragmento calculada com uma combinação dos termos difuso,
+    // especular, e ambiente. Veja slide 129 do documento Aula_17_e_18_Modelos_de_Iluminacao.pdf.
+    color = Kd0*(lambert_diffuse_term + ambient_term + phong_specular_term);
     }
 
-
-    // Cor final com correção gamma, considerando monitor sRGB.
+        // Cor final com correção gamma, considerando monitor sRGB.
     // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
     color = pow(color, vec3(1.0,1.0,1.0)/2.2);
-
 
 } 
 
